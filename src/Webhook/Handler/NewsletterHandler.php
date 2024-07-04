69a1a391af8fb4a0b66319666bfc868fda726f7b
Gary Clarke <gary.clarke@immediate.co.uk>
2024-07-04 18:52:13 +0100

    Start considering deserializing into newsletter dto

--- src/Webhook/Handler/NewsletterHandler.php
+++ src/Webhook/Handler/NewsletterHandler.php
@@ -8,7 +8,7 @@ use App\DTO\Webhook;
 
 class NewsletterHandler implements WebhookHandlerInterface
 {
-    private const array SUPPORTED_EVENTS = ['newsletter_opened', 'newsletter_subscribed', 'newsletter_unsubscribed'];
+    private const SUPPORTED_EVENTS = ['newsletter_opened', 'newsletter_subscribed', 'newsletter_unsubscribed'];
 
     public function supports(Webhook $webhook): bool
     {
@@ -17,6 +17,6 @@ class NewsletterHandler implements WebhookHandlerInterface
 
     public function handle(Webhook $webhook): void
     {
-        dd($webhook);
+        $newsletterDto = $this->newsletterDtoFactory->createFromWebhook($webhook);
     }
 }
