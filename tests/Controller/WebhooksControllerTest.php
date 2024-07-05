2b4f114631713e0a9155d86761d89eeb361f08d9
Gary Clarke <gary.clarke@immediate.co.uk>
2024-07-05 16:02:15 +0100

    Sketch out test steps

--- tests/Controller/WebhooksControllerTest.php
+++ tests/Controller/WebhooksControllerTest.php
@@ -4,27 +4,48 @@ declare(strict_types=1);
 
 namespace Controller;
 
+use Symfony\Bundle\FrameworkBundle\KernelBrowser;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpFoundation\Response;
 
 class WebhooksControllerTest extends WebTestCase
 {
+    private KernelBrowser $webTester;
+    private ContainerInterface $container;
+
+    protected function setUp(): void
+    {
+        $this->webTester = static::createClient();
+        $this->container = $this->webTester->getContainer();
+    }
+
     public function testWebhooksAreHandled(): void
     {
-        $webTester = static::createClient();
+        $webhook = '{"event": "newsletter_subscribed","timestamp": "2023-07-03T12:00:00Z","user": {"client_id":"some-user-guid","email": "user@example.com","region": "UK"},"newsletter": {"newsletter_id":"newsletter-001","topic": "Summer Sales","product_id":"some-product-identifier"}}';
 
-        $webhook = '{"event": "newsletter_opened","timestamp": "2023-07-03T12:00:00Z","user": {"client_id":"some-user-guid","email": "user@example.com","region": "UK"},"newsletter": {"newsletter_id":"newsletter-001","topic": "Summer Sales","product_id": "some-product-identifier"}}';
+        $this->postJson($webhook);
 
-        $webTester->request(
+        // Assert CdpClient::track() called once
+        // Assert correct TrackModel is passed to CdpClient::track() method
+        // Assert TrackModel::toArray() organizes data into format expected by CDP
+        // Assert CdpClient::identify() called once
+        // Assert correct IdentifyModel is passed to CdpClient::identify() method
+        // Assert IdentifyModel::toArray() organizes data into format expected by CDP
+
+        $this->assertEquals(Response::HTTP_NO_CONTENT, $this->webTester->getResponse()->getStatusCode());
+    }
+
+    private function postJson(string $payload): void
+    {
+        $this->webTester->request(
             method: 'POST',
             uri: '/webhook',
             server: [
                 'CONTENT_TYPE' => 'application/json',
                 'HTTP_ACCEPT' => '*/*',
             ],
-            content: $webhook
+            content: $payload
         );
-
-        $this->assertEquals(Response::HTTP_NO_CONTENT, $webTester->getResponse()->getStatusCode());
     }
 }
