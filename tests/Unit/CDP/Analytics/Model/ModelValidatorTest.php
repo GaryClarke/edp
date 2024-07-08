a6ea7511e98df78c64769df069e6da023e262a9a
Gary Clarke <gary.clarke@immediate.co.uk>
2024-07-08 19:00:19 +0100

    Create ModelValidatorTest

---
+++ tests/Unit/CDP/Analytics/Model/ModelValidatorTest.php
@@ -0,0 +1,63 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Unit\CDP\Analytics\Model;
+
+use App\CDP\Analytics\Model\ModelValidator;
+use App\CDP\Analytics\Model\Subscription\Identify\IdentifyModel;
+use App\Error\Exception\WebhookException;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\Validator\Validation;
+
+class ModelValidatorTest extends TestCase
+{
+    private ModelValidator $unit;
+
+    public function setUp(): void
+    {
+        $validator = Validation::createValidatorBuilder()
+            ->enableAttributeMapping()
+            ->getValidator();
+
+        $this->unit = new ModelValidator($validator);
+    }
+
+    public function testInvalidIdentifyModelFailsValidation(): void
+    {
+        $model = new IdentifyModel();
+        $model->setProduct('');
+        $model->setEmail('not-an-email');
+        $model->setEventDate('12-12-2001');
+        $model->setSubscriptionId('12334');
+        $model->setId('some-id');
+
+        try {
+            $this->unit->validate($model);
+            $this->fail('No exception was thrown');
+        } catch (WebhookException $exception) {
+            $this->assertInstanceOf(WebhookException::class, $exception, 'Expected WebhookException was thrown');
+            $this->assertEquals(
+                'Invalid IdentifyModel properties: product, eventDate, email',
+                $exception->getMessage()
+            );
+        }
+    }
+
+    public function testValidIdentifyModelFailsValidation(): void
+    {
+        $model = new IdentifyModel();
+        $model->setProduct('some-product');
+        $model->setEmail('email@example.com');
+        $model->setEventDate('2025-01-01');
+        $model->setSubscriptionId('12334');
+        $model->setId('some-id');
+
+        try {
+            $this->unit->validate($model);
+            $this->assertTrue(true, 'No WebhookException was thrown'); // force test to pass
+        } catch (WebhookException) {
+            $this->fail('Unexpected exception thrown');
+        }
+    }
+}
